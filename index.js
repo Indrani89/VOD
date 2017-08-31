57294c11ecfa10993c3a9394de49ff02d9e0219a
Indrani <indu_psg2008@yahoo.com>
2017-09-01 08:34:35 +1200

    Added title and commented mlab db configuration

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 var config= require('./config');
 var server=require('./server');
 //var mongo = require('mongoskin');
-
 //var dbConfig =  config.dbProduction; 
-
 //var db = mongo.db('mongodb://'+ dbConfig.mongo.url +'/' + dbConfig.mongo.database , { safe:true });
 //console.log("db->"+db);
-
 //create a short cut to convert id to object Id
 //db.toObjectId = mongo.ObjectID.createFromHexString;
 
